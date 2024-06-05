e49f96bfde3ca6a1c3629301afadda7d7b71058c
Rojan Rana Magar <rozenmagar058@gmail.com>
2024-06-05 15:14:21 +0545

    chapter: completed sublist

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -6,6 +6,26 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    if first_list == second_list {
+        return Comparison::Equal;
+    } else if is_sublist(first_list, second_list) {
+        return Comparison::Sublist;
+    } else if is_sublist(second_list, first_list) {
+        return Comparison::Superlist;
+    } else {
+        return Comparison::Unequal;
+    }
+}
+
+fn is_sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
+    if first_list.is_empty() {
+        return true;
+    }
+    if first_list.len() > second_list.len() {
+        return false;
+    }
+    second_list
+        .windows(first_list.len())
+        .any(|window| window == first_list)
 }
